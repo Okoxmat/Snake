10c5ed0bb1db3f9ae08f331548a9e8b17017d7ae
Okoxmat <til.f.eichler@gmail.com>
2018-08-04 08:33:40 +0200

    Erste Schritte
    Zeile 70 in MainWindow.xaml.cs "Playground" kann nicht als Name des <Grid> aus MainWindow.xaml erkannt werden.

---
+++ Snake/Snake/Felder_Eigenschaften.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public enum FeldEigenschaft
+    {
+        /// <summary>
+        /// Graue Wand
+        /// </summary>
+        Barriere,
+        /// <summary>
+        /// Brauner Kopf
+        /// </summary>
+        Kopf,
+        /// <summary>
+        /// Dunkelgrüner Körper
+        /// </summary>
+        Körper,
+        /// <summary>
+        /// weißes Feld
+        /// </summary>
+        Leer,
+        /// <summary>
+        /// Rotes Feld (Apfel)
+        /// </summary>
+        Essen
+    }
+}
---
+++ Snake/Snake/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Snake
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region eigenes Zeug
+
+        /// <summary>
+        /// Anzahl der Schlange ohne Kopf
+        /// </summary>
+        private FeldEigenschaft[] LängeDerSchlange;
+
+        /// <summary>
+        /// Speichert zu jedem Feld die Eigenschaften
+        /// </summary>
+        private FeldEigenschaft[] AlleFelder;
+
+        /// <summary>
+        /// Wenn true gerade gegessen
+        /// </summary>
+        private bool[] Essengegessen;
+
+        /// <summary>
+        /// Wenn true Spiel zu Ende 
+        /// </summary>
+        private bool GameOver;
+
+        #endregion
+
+
+
+
+        #region Spawn
+        /// <summary>
+        /// Spawn
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            NewGame();
+        }
+
+
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+        private void NewGame()
+        {
+            //Array mit 256 leeren Feldern
+            AlleFelder = new FeldEigenschaft[256];
+
+            for (var i = 0; i < AlleFelder.Length; i++)
+            {
+                AlleFelder[i] = FeldEigenschaft.Leer;
+            }
+
+            //Felder zurücksetzen
+
+            Playground.Children.Cast<Canvas>().ToList().ForEach(canvas =>
+            {
+                canvas.Background = Brushes.White;
+            }
+            );
+        }
+        #endregion
+    }
+}
